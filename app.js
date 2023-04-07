e710fc1830958d3111d7b910e1673b0a47b87c6a
Isra Zahid <59427634+israzahid@users.noreply.github.com>
2023-04-07 18:46:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,28 +12,6 @@ app.get("/", function(req, res) {
     
 });
 
-app.post("/", function(req, res) {
-    const query = req.body.cityName;
-    const apiKey = "APIKEY";
-    const units = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "3&units=" + units
-    https.get(url, function(response){
-        console.log(response);
-
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp
-            const description = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-            res.write("<p>The weather is currently " + description + "</p>")
-            res.write("<h1>The temperature in " + query + " is "+ temp + " degrees Celcius</h1>");
-            res.write("<img src=" + imageURL + ">")
-            res.send();
-        })
-    })
-});
-
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000");
